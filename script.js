8453a1a2e88de675ee750198ba9df5c1ae6a2370
Helen Chen <hc20@princeton.edu>
2020-07-06 17:55:57 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+const lTetromino = "firstShape"
+const zTetromino = "secondShape"
+const oTetromino = "thirdShape"
+const iTetromino = "fourthShape"
+const tTetromino = "fifthShape"
+
+
+const tetrominoes = [lTetromino, zTetromino, oTetromino, iTetromino, tTetromino];
+
+// this happens before hello, tetris in app.js
+console.log(tetrominoes[0]);
